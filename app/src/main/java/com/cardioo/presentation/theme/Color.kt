ba0a94ff327e2605c6b65e832df362fea68a62f7
Ivan Grigoryev <igrigoryev@pmsoft.ru>
2026-04-03 04:36:17 +0300

    Make primary color a bit lighter

--- app/src/main/java/com/cardioo/presentation/theme/Color.kt
+++ app/src/main/java/com/cardioo/presentation/theme/Color.kt
@@ -2,7 +2,7 @@ package com.cardioo.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PinkPrimary = Color(0xFFFF6B8B)
+val PinkPrimary = Color(0xFFFF6F8E)
 val PinkContainer = Color(0xFFFFD9E1)
 val PinkAccent = Color(0xFFFFA2B5)
 
