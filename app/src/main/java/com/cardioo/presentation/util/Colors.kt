d031075029b1b3c56cd911add06516cfd5b38a6a
Ivan Grigoryev <igrigoryev@pmsoft.ru>
2026-04-03 04:34:06 +0300

    Change border color

--- app/src/main/java/com/cardioo/presentation/util/Colors.kt
+++ app/src/main/java/com/cardioo/presentation/util/Colors.kt
@@ -2,11 +2,11 @@ package com.cardioo.presentation.util
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.cardioo.domain.model.BpCategory
-import com.cardioo.presentation.theme.PinkPrimary
 
 
 val Orange = Color(0xFFFFA726);
@@ -14,7 +14,7 @@ val Bordeaux = Color(0xFF911535);
 
 @Composable
 fun toggleButtonBorder(toggle: Boolean): BorderStroke {
-    val toggledButtonBorder = BorderStroke(2.dp, PinkPrimary)
+    val toggledButtonBorder = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
     val untoggledButtonBorder = ButtonDefaults.outlinedButtonBorder(true)
     return if (toggle) toggledButtonBorder
     else untoggledButtonBorder
